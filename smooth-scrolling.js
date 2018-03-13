992dceb6768faca3117fc3a5b94e13316cfafaf2
ryanthnguyen <qtquoc@memphis.edu>
2018-03-13 15:18:50 -0500

    implemented projects and implemented smooth scrolling

---
+++ smooth-scrolling.js
@@ -0,0 +1,13 @@
+$('a.scrollTo').on('click',function(){
+  var scrollTo = $(this).attr('data-scrollTo');
+  if (scrollTo === 'homeloc') {
+    console.log('homeloc activated');
+    $('body,html').animate({
+      "scrollTop": $('#'+scrollTo).offset().top-500},1000);
+  } else {
+    console.log('not homeloc');
+    $('body,html').animate({
+      "scrollTop": $('#'+scrollTo).offset().top-40},1000);
+  }
+  return false;
+})
